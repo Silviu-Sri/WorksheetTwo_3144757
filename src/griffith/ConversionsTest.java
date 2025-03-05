3b81f7392e4375086b5409662ca1a83db8c9bccf
Serianu Andrei-Silviu <andreisilviu.serianu@student.griffith.ie>
2025-03-05 11:55:07 +0000

    Dollar to Euro Fail

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -12,4 +12,12 @@ public class ConversionsTest {
         assertEquals(5.40, conv.euroToDollar(5), 0.01);
         assertEquals(0.00, conv.euroToDollar(0), 0.01);
     }
+    
+    @Test
+    public void testDollarToEuro() {
+        Conversions conv = new Conversions();
+        assertEquals(0.93, conv.dollarToEuro(1), 0.01);
+        assertEquals(4.65, conv.dollarToEuro(5), 0.01);
+        assertEquals(0.00, conv.dollarToEuro(0), 0.01);
+    }
 }
